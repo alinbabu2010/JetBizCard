7a951280c2ebb6a049b939f68e44aef1e8efa1b0
Alin Babu <alinbabu2010@gmail.com>
2022-01-15 12:06:21 +0530

    Add card view nad its properties

--- app/src/main/java/com/compose/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/compose/jetbizcard/MainActivity.kt
@@ -3,13 +3,16 @@ package com.compose.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +31,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier
-        .fillMaxHeight()
-        .fillMaxWidth()) {
-
+    Surface(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            elevation = 4.dp
+        ) {
+        }
     }
 }
 
