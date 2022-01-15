77c51e34082b3d11f2d1931c9764cf0862bc9dc9
Alin Babu <alinbabu2010@gmail.com>
2022-01-15 12:34:10 +0530

    Add column and divider. Also refactor code in MainActivity.kt

--- app/src/main/java/com/compose/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/compose/jetbizcard/MainActivity.kt
@@ -10,9 +10,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -50,22 +52,32 @@ fun CreateBizCard() {
             elevation = 4.dp
         ) {
 
-            Surface(
-                modifier = Modifier
-                    .size(154.dp)
-                    .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp, Color.LightGray),
-                elevation = 4.dp,
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_user),
-                    contentDescription = "Profile Image",
-                    modifier = Modifier.size(135.dp)
-                )
+            Column(modifier = Modifier.height(300.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                CreateImageProfile()
+                Divider()
             }
         }
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(154.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_user),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp)
+        )
     }
 }
 
