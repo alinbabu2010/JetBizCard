bea1374b24f3290f595dbeb33763793d0c6ff61d
Alin Babu <alinbabu2010@gmail.com>
2022-01-15 12:21:39 +0530

    Add profile image

--- app/src/main/java/com/compose/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/compose/jetbizcard/MainActivity.kt
@@ -3,7 +3,10 @@ package com.compose.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -11,6 +14,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.jetbizcard.ui.theme.JetBizCardTheme
@@ -44,6 +49,22 @@ fun CreateBizCard() {
             shape = RoundedCornerShape(corner = CornerSize(16.dp)),
             elevation = 4.dp
         ) {
+
+            Surface(
+                modifier = Modifier
+                    .size(154.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_user),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier.size(135.dp)
+                )
+            }
         }
     }
 }
